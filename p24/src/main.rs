150f62c664ad2a9abe6fbe52a6ecdd54ca8bee38
Aliaksei Kutsevol <Aliaksei.Kutsevol@tii.ae>
2024-06-09 02:33:01 +0400

    fixed code

--- p24/src/main.rs
+++ p24/src/main.rs
@@ -1,21 +1,49 @@
-mod refs;
-use crate::refs::{f1,f2,f3,f4};
-
 fn main() {
-    println!("Hello, world!");
-    let mut tuple = (10, 20, false);
-    let res_1 = f1(&mut tuple);
-    println!("{:?}", res_1.unwrap());
+    let arr = [2, 4, 8, 16];
 
-    let mut slice = [10, 20, 30, 40];
-    let res_2 = f2(&mut slice, 2);
-    println!("{:?}", res_2.unwrap());
+    let mut n = 2;
+    let nth = nth_item(&arr, n);
+    let increased = increased_by_first_item(&arr, &mut n);
 
-    let mut slice = [10, 20, 30, 40];
-    let res_3 = f3(&mut slice, 1);
-    println!("{:?}", res_3.unwrap());
+    let value = {
+        let values = TwoValues::new(&arr[3], increased);
 
-    let slice = [10, 20, 30, 40, 50, 60, 70, 80];
-    let res_4 = f4(&slice);
-    println!("{:?}", res_4);
+        assert_eq!(values.get_first(), &16);
+
+        values.get_second()
+    };
+
+    assert_eq!(*value, 4);
+    assert_eq!(*nth, 8);
+
+    println!("*value - {}", *value);
+    println!("*nth - {}", *nth);
+}
+
+fn nth_item<'a>(data: &'a [usize], n: usize) -> &'a usize {
+    &data[n]
+}
+
+fn increased_by_first_item<'a>(data: &[usize], n: &'a mut usize) -> &'a mut usize {
+    *n += data[0];
+    n
+}
+
+struct TwoValues<'a> {
+    first: &'a usize,
+    second: &'a usize,
+}
+
+impl<'a> TwoValues<'a> {
+    pub fn new(first: &'a usize, second: &'a usize) -> Self {
+        Self { first, second }
+    }
+
+    pub fn get_first(&self) -> &'a usize {
+        self.first
+    }
+
+    pub fn get_second(&self) -> &'a usize {
+        self.second
+    }
 }
